82d8f5f410f4d8d2963a44ddcd1c60daa5565df6
sharon0511 <sharon05110511@gmail.com>
2023-10-30 16:32:31 +0900

    coin converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  // const [coinIndex, setCoinIndex] = useState("BTC");
+  const [exchangeRate, setExchangeRate] = useState(0);
+  const [money, setMoney] = useState(0);
+
+  const onSelect = (event) => {
+    setExchangeRate(parseInt(event.target.value));
+  }
+
+  const onChange = (event) => {
+    setMoney(event.target.value);
+    console.log(money)
+  }
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {(loading ? null : `(${coins.length})`)}</h1>
+      {loading ? <strong>Loading..</strong> : (<div>
+        <div>
+          <select onChange={onSelect}>
+            <option value={0}>Select</option>
+            {coins.map((coin) => (
+              <option value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          USD: <input type="number" value={money} onChange={onChange}></input>
+        </div>
+        <div>
+          Coin: <input type="number" value={money / exchangeRate} readOnly></input>
+        </div>
+      </div>)}
+      {/* <ul>
+        {coins.map((coin, index) => <li key={index}>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</li>)}
+      </ul> */}
     </div>
   );
 }
